416d5773ada88418eb77d7fc9f2f65d916ea264e
iOSatya <125645326+iOSatya@users.noreply.github.com>
2025-04-28 09:47:44 +0700

    Update

--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -1,9 +1,10 @@
 <template>
 
   <form class="flex flex-col">
+    <input type="text" v-model="registerForm.name" placeholder="Name" class="form-control">
     <input type="email" v-model="registerForm.email" placeholder="E-Mail" class="form-control">
     <input type="password" v-model="registerForm.password" placeholder="Password" class="form-control">
-    <input type="password" v-model="registerForm.password_confirmation" placeholder="Password" class="form-control">
+    <input type="password" v-model="registerForm.password_confirmation" placeholder="Password Confirmation" class="form-control">
     <div class="flex">
       <RouterLink :to="{name: 'login'}">Login</RouterLink>
       <button type="submit" class="button">Register</button>
@@ -17,6 +18,7 @@
   import { ref } from 'vue';
 
   const registerForm = ref({
+    name: "",
     email: "",
     password: "",
     password_confirmation: ""
---
+++ server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        try {
+            $data = $request->validate([
+                "name" => ["required"],
+                "email" => ["required", "email"],
+                "password" => ["required"]
+            ]);
+
+            User::create($data);
+
+            return response()->json(["message" => "Register Success"], 201);
+
+        } catch (Exception $error) {
+            return response()->json(["message" => $error->getMessage()], 400);
+        }
+    }
+
+    public function login(Request $request) {
+        try {
+            $request->validate([
+                "email" => ["required", "email"],
+                "password" => ["required"]
+            ]);
+
+            $user = User::where("email", $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json(["message" => "Invalid Credentials"], 401);
+            }
+
+            $token = $user->createToken($user->name)->plainTextToken;
+
+            return response()->json(["message" => "Login Success", "token" => $token], 200);
+
+        } catch (Exception $error) {
+            return response()->json(["message" => $error->getMessage()], 500);
+        }
+    }
+
+    public function logout(Request $request) {
+        try {
+            $request->user()->currentAccessToken()->delete();
+            return response()->json(["message" => "Logout Success"], 200);
+        } catch (Exception $error) {
+            return response()->json(["message" => $error->getMessage()], 500);
+        }
+        
+    }
+}
