97c23804f99bc0587749138838b2a992bc1b508f
KalfinSyah <kalfinsyah.km@gmail.com>
2025-05-15 13:13:46 +0700

    delete comment

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,3 @@
-<!-- <template>
-
-  <RouterView />
-
-</template>
-
-<style>
-
-  .form-control {
-    border-bottom: 1px solid var(--secondary);
-    padding: 0.5rem;
-    outline: none;
-  }
-
-  .button {
-    display: block;
-    cursor: pointer;
-  }
-
-</style> -->
-
-<!-- ------------------------------------------------------------------------------------------------------------------------------ -->
- 
 <template>
   <Navigator v-if="!hideNavbar" />
   <RouterView />
